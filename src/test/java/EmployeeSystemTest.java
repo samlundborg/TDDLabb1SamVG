62175db714764a19bd83db0ffa3941d94a217dc0
samlundborg <sagaremus97>
2021-02-10 20:17:17 +0100

    First red, compiler error

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+
 public class EmployeeSystemTest {
 
     @Test
     @DisplayName("Yeeeey")
-    void test() {
+    void testForCreatingEmployeeSystem() {
+        EmployeeSystem testObject = new EmployeeSystem();
+        Employee testPersonal = new Employee("Adam", "Ballard", 26, 45000);
 
     }
 }
